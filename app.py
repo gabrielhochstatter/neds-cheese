ba2af3943d1f02abb7b36652417561248949e039
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-07-01 17:46:40 +0100

    working using templates

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from model import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
-    return 'Your IP is {0}'.format(eur_to_usd_rate)
+    cheese_amount = calculate_cheese(19.99, 3.24, gbp_to_usd_rate, eur_to_usd_rate)
+    return render_template('index.html', cheese_amount=cheese_amount)
