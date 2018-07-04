c1749b458c1d2dcdfef3f8d08d007bfae926fae4
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-07-04 13:25:48 +0100

    redid homepage to have country selection

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/shop')
+def shop():
     cheese_amount = calculate_cheese(19.99, 3.24, gbp_to_usd_rate, eur_to_usd_rate)
-    return render_template('index.html', cheese_amount=cheese_amount)
+    return render_template('shop.html', cheese_amount=cheese_amount)
